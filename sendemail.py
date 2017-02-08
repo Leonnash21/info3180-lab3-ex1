a2251685353248b2a32f705757f8ae87928bc7c0
leonnash21 <leonnash2008@gmail.com>
2017-02-08 06:10:10 +0000

    changes made

---
+++ sendemail.py
@@ -0,0 +1,24 @@
+import smtplib
+
+
+from_name = 'Neil'
+from_addr = 'yourusername@gmail.com' 
+to_name ='Nash'
+to_addr = 'receiverusername@gmail.com' 
+subject = 'SMTP email test'
+message = """From: {} <{}>
+To: {} <{}> 
+Subject: Testing SMTP
+Be enlightened
+"""
+
+message_to_send = message.format(from_name, from_addr, to_name, to_addr, subject, message)
+# Credentials (if needed)
+username = 'yourusername@gmail.com' 
+password = '{youremailapppassword}'
+# The actual mail send
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username, password)
+server.sendmail(from_addr, to_addr, message_to_send)
+server.quit()
